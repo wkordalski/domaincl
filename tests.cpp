3fb0a533dbe074d20d34c3944f327375fcde6da4
Franciszek Piszcz <franekpiszcz95@gmail.com>
2014-12-08 18:38:46 +0100

    added some more usage examples

---
+++ tests.cpp
@@ -0,0 +1,105 @@
+/**
+	Copyright (C) 2014 Franciszek Piszcz
+	
+	Distributed under the GNU Lesser General Public License, version 3
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+#include "headers.h"
+#include "api_abstr.h"
+#include "dsl.h"
+
+using namespace domaincl;
+namespace api_abstr_example
+{
+	void run()
+	{
+		using namespace api_abstr;
+		scope gpu("GPU");
+		vector<int> v1(30),v2(30),v3(30);
+		for(int i=0;i<v1.size();++i)
+		{
+			v1[i] = i % 10;
+			v2[i] = i % 10;
+			v3[i] = 0;
+		}
+		gpu.send_buf("_v1",(void*)&(v1[0]),v1.size()*sizeof(int));
+		gpu.send_buf("_v2",(void*)&(v2[0]),v2.size()*sizeof(int));
+		gpu.send_buf("_v3",(void*)&(v3[0]),v3.size()*sizeof(int));
+		gpu.send_int("size",v1.size());
+		#define STRINGIFY(a) #a
+		string kod = STRINGIFY(
+			__global int* v1 = (__global int*)_v1;
+			__global int* v2 = (__global int*)_v2;
+			__global int* v3 = (__global int*)_v3;
+			v3[i] = v1[i] + v2[i];
+		);
+		#undef STRINGFY
+		gpu.compile_prog("vec_add",make_pair((string)"i",(string)"size"),kod);
+		gpu.run_prog("vec_add");
+		gpu.fetch_buf("_v3",(void*)&(v3[0]));
+		for(int i=0;i<v3.size();++i)
+		cout<<v3[i]<<" ";
+		cout<<endl;
+	}
+}
+
+namespace dsl_example
+{
+	void run()
+	{
+		using namespace dsl;
+		vector<int> a,b,c;
+		float wsp_glob = 10.3;
+		int func_const1 = 25;
+		int func_const2 = 999999;
+		auto func = [&](val i)
+		{
+			DO(
+			val aa = a;
+			val bb = b;
+			val cc = c;
+			val j = val(0)+val(0);
+			DO(
+				cc[i] = aa[i] + bb[i];
+				If(cc[i] == val(10)) DO(
+					cc[i] = val(999999);
+				) Else DO(
+					cc[i] = 0;
+					For(j, 1, aa[i]) DO(
+						cc[i] = cc[i] + aa[i];
+					)
+				)
+			)
+			)
+		};
+		kernel k = function<void(val)>(func);
+		a.resize(10,0);
+		b.resize(10,0);
+		c.resize(10,0);
+		for(int i=0;i<10;++i)
+		{
+			a[i] = i;
+			b[i] = i;
+		}
+		k.copyin();
+		k.run(10);
+		k.copyout();
+		for(int i=0;i<10;++i)
+		cout<<c[i]<<" ";
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	dsl_example::run();
+	return 0;
+}
